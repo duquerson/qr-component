35eae44be472532c91f8e25e8ef76286945cfb4e
Duquerson <sotodjgato@hotmail.com>
2022-10-16 22:19:32 -0300

    cambios de pocicionamiento y combinacion de estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        Fraunces: ["Fraunces", "arial"]
+      },
+      colors: {
+        blanco: 'hsl(0, 0%, 100%)',
+        gris_claro: 'hsl(212, 45%, 89%)',
+        gris_oscuro: 'hsl(220, 15%, 55%)',
+        azul: 'hsl(218, 44%, 22%)',
+      }
+    },
   },
   plugins: [],
 }
